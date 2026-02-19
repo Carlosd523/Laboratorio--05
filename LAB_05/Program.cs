374c901c25ab75882356210409ea6999d2a9b93e
Carlos Angulo <cdangulo08@gmail.com>
2026-02-18 20:33:09 -0600

    Ejercicio 1 - Agregar la solución
    Se realiza el ejercicio 1, en el cual se determina si un número es positivo, negativo o cero

---
+++ LAB_05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // Entradas - Se le pide al usuario que ingrese un número entero.
+        Console.WriteLine("Ingrese un número entero");
+        string numero = Console.ReadLine();
+        int num = int.Parse(numero!);
+
+        //Proceso - El número que ingresó el usuario será evaluado para determinar si es positivo, negativo o es cero.
+        if (num > 0)
+        {
+            // Posible salida número 1
+            Console.WriteLine("El número ingresado es positivo");
+        }else if(num < 0)
+        {
+            // Posible salida número 2
+            Console.WriteLine("El número ingresado es negativo");
+        }
+        else
+        {
+            // Posible salida número 3
+            Console.WriteLine("El número ingresado es cero");
+        }
+
+    }
+}
