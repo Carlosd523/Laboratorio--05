2af2a9b904a68253c5e88402485f1c402aa068f6
Carlos Angulo <cdangulo08@gmail.com>
2026-02-18 20:44:20 -0600

    Agregación - Ejercicio #2
    Se agrega el ejercicio número dos, en el cual se determina si un año es bisiesto o no. También se cambia de lugar la diferenciación de ejercicios, puesto que, estaban fuera del Main.

--- LAB_05/Program.cs
+++ LAB_05/Program.cs
@@ -3,7 +3,7 @@ class Program
 {
     static void Main()
     {
-        // Ejercicio número 1
+        // Ejercicio número 1 - Determinar si un número es positivo, negativo o cero
 
         // Entradas - Se le pide al usuario que ingrese un número entero.
         Console.WriteLine("Ingrese un número entero");
@@ -26,15 +26,38 @@ class Program
             Console.WriteLine("El número ingresado es cero");
         }
 
-    }
+        // Ejercicio número 2 - Determinar si un año es bisiesto
 
-        // Ejercicio número 2
+        //Entradas - Se le pide al usuario que agregue el año a determinar
+        Console.WriteLine("Ingrese el año que quiera evaluar");
+        string time = Console.ReadLine();
+        int year = int.Parse(time!);
 
-
+        //Proceso - Determinar si el año ingresado es bisiesto o no
+        if (year % 400 == 0)
+        {
+            // Posible salida 1
+            Console.WriteLine("El año sí es bisiesto");
+        }else if (year % 100 == 0)
+        {
+            // Posible salida 2
+            Console.WriteLine("El año no es bisiesto");
+        }else if(year % 4 == 0)
+        {
+            // Posible salida 3
+            Console.WriteLine("El año sí es bisiesto");
+        }
+        else
+        {
+            //Posible salida 4
+            Console.WriteLine("El año no es bisiesto");
+        }
 
         //Ejercicio número 3
 
 
 
         // Ejercicio número 4
+
+    }
 }
